e18d572b1b8d5570f6c04815042a4df748d4765e
Sasha <2209sashaz@gmail.com>
2022-10-15 21:49:27 +0300

    Done task 7

---
+++ module 7/Отрезок.py
@@ -0,0 +1,11 @@
+a = int(input('Введите число: '))
+b = int(input('Введите число: '))
+medium_of_num = 0
+count = 0
+for i in range(a, b):
+    if i % 3 == 0:
+        medium_of_num += i
+        count += 1
+medium_of_num /= count
+print('Среднее арифметическое чисел, кратных 3, равно', medium_of_num)
+
