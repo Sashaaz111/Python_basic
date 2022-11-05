de26caf8111239e5631f0cea6e2a279ce97fadf9
Sasha <2209sashaz@gmail.com>
2022-11-06 00:48:20 +0300

    Done task 3

--- module 10/Рамка.py
+++ module 10/Рамка.py
@@ -2,9 +2,9 @@ width = int(input('Введите ширину: '))
 height = int(input('Введите высоту: '))
 for row in range(1, width + 1):
     for col in range(1, height + 1):
-        if col == 1 or col == height:
+        if col == 1 or col == width:
             print('|', end = '')
-        elif row == 1 or row == width:
+        elif row == 1 or row == height:
             print('-', end = '')
         else:
             print(' ', end = '')
